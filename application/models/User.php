9e21ad9270dcb09390eacd1d7d414aa59a713857
HYEZ <oohyejioo@gmail.com>
2019-07-16 13:36:08 +0900

    # 값이 empty일 경우 fail

--- application/models/User.php
+++ application/models/User.php
@@ -11,36 +11,37 @@ class User extends CI_Model {
 
     /* User Login */
     public function login($argu) {
-      $this->db->where('id', $argu['id']);
-      $this->db->where('pw', $argu['pw']);
-      $this->db->select("*");
-      $this->db->from("user");
-      $result = $this->db->get();
-      $data = '';
-      foreach( $result->result() as $row )
-      {
-        $data = $row->idx;
+      if(empty($argu['id']) || empty($argu['pw']) {
+        return array('status' => API_EMPTY_PARAMS, 'data' => null);
+      } else {
+        $this->db->where('id', $argu['id']);
+        $this->db->where('pw', $argu['pw']);
+        $this->db->select("*");
+        $this->db->from("user");
+        $result = $this->db->get();
+        $data = '';
+        foreach( $result->result() as $row )
+        {
+          $data = $row->idx;
+        }
+        return $data;
       }
-      return $data;
     }
 
      /* User Join */
     public function insert($argu) {
-      if(empty($argu['id'])) {
-        return 400;
+      if(empty($argu['id']) || empty($argu['pw']) || empty($argu['name']) || empty($argu['gender']) || empty($argu['gender'])) {
+        return API_EMPTY_PARAMS;
       } else {
-        
+        $this->db->set('id', $argu['id']);
+        $this->db->set('pw', $argu['pw']);
+        $this->db->set('name', $argu['name']);
+        $this->db->set('gender', $argu['gender']);
+        $this->db->set('birth', $argu['birth']);
+        $this->db->insert("user");
+        $result = $this->db->get();
+      
+        return $this->db->insert_id();
       }
-      $this->db->set('id', $argu['id']);
-      $this->db->set('pw', $argu['pw']);
-      $this->db->set('name', $argu['name']);
-      $this->db->set('gender', $argu['gender']);
-      $this->db->set('birth', $argu['birth']);
-      $this->db->insert("user");
-      $result = $this->db->get();
-    
-      return $this->db->insert_id();
     }
-
-
 }
