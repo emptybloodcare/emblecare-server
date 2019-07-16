76ed7123464eb67b9863edc4442b30d5aa08a116
HYEZ <oohyejioo@gmail.com>
2019-07-16 13:23:28 +0900

    # 회원가입 모델 개발 중

--- application/models/User.php
+++ application/models/User.php
@@ -24,4 +24,23 @@ class User extends CI_Model {
       return $data;
     }
 
+     /* User Join */
+    public function insert($argu) {
+      if(empty($argu['id'])) {
+        return 400;
+      } else {
+        
+      }
+      $this->db->set('id', $argu['id']);
+      $this->db->set('pw', $argu['pw']);
+      $this->db->set('name', $argu['name']);
+      $this->db->set('gender', $argu['gender']);
+      $this->db->set('birth', $argu['birth']);
+      $this->db->insert("user");
+      $result = $this->db->get();
+    
+      return $this->db->insert_id();
+    }
+
+
 }
