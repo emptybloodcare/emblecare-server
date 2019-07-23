24280c990f3619f02756dbeadb2d136c9c8e42a8
HYEZ <oohyejioo@gmail.com>
2019-07-23 18:04:27 +0900

    날씨 api 새로운거

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -80,7 +80,7 @@ class Api extends CI_Controller {
 		$results = $result->body;
 		$bl_data='';
 		$result = [];
-		
+
 		foreach($results->data as $item) {
 			if(!$bl_data) {
 				$bl_data=true;
@@ -95,6 +95,27 @@ class Api extends CI_Controller {
 		echo json_encode($result);
 	}
 	
+	public function weather2() {
+		$url = 'http://google.com';
 
+		
+		if ($argc > 1){
+		    $url = $argv[1];
+		}
+		$ch=curl_init();
+		// user credencial
+		curl_setopt($ch, CURLOPT_USERPWD, "username:passwd");
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_VERBOSE, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		var_dump($response);
+	}
 
 }
